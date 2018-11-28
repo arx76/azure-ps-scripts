5cb2b5cc8cc00a0e81bcca390a9209712efbf38c
Arvinder <30565401+arvinder-dhar@users.noreply.github.com>
2018-11-28 20:20:18 +0530

    Add files via upload

---
+++ Azure_Vnet_Peering.ps1
@@ -0,0 +1,14 @@
+﻿# Description : Peer two vnets named "arvinder-vnet" and "ruchi-vnet"
+
+# Load arvinder-vnet and ruchi-vnet into Variables
+$vneta = Get-AzureRmVirtualNetwork -Name "arvinder-vnet" -ResourceGroupName "arvinderrg"
+$vnetb = Get-AzureRmVirtualNetwork -Name "ruchi-vnet" -ResourceGroupName "ruchirg"
+
+# Peer arvinder-vnet to ruchi-vnet
+Add-AzureRmVirtualNetworkPeering -Name 'Arvinder-Ruchi' -VirtualNetwork $vneta -RemoteVirtualNetworkId $vnetb.Id
+
+# Peer VNETB to VNETA
+Add-AzureRmVirtualNetworkPeering -Name 'Ruchi-Arvinder' -VirtualNetwork $vnetb -RemoteVirtualNetworkId $vneta.Id
+
+#Check on the Peering Status  
+Get-AzureRmVirtualNetworkPeering -ResourceGroupName Arvinderrg -VirtualNetworkName "arvinder-vnet" | Format-Table VirtualNetworkName, Name, PeeringState
